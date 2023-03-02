3899de8d2474400c43e458219ec2dcb893805d39
KUNAL <KUNAL@DESKTOP-LCADD3G>
2023-03-03 00:34:05 +0530

    Does S Contains T

---
+++ src/dsaTest1/doesSContainsT.java
@@ -0,0 +1,31 @@
+package dsaTest1;
+
+public class doesSContainsT {
+	
+	public static boolean checkSequence(String a, String b) {
+		if(b.length()==0) {
+			return true;
+		}if(a.length()==0) {
+			return false;
+		}
+		if(a.charAt(0) == b.charAt(0)){
+            a = a.substring(1);
+            b = b.substring(1);
+        }else{
+            a = a.substring(1);
+        }
+        
+        boolean ans = checkSequence(a,b);
+
+        return ans;
+		
+	}
+
+	public static void main(String[] args) {
+		String s = "kunal";
+		String t = "kur";
+		System.out.println(checkSequence(s,t));
+
+	}
+
+}
