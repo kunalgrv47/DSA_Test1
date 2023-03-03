314b3a8dae480432968d55a906ea09cbb4f6d739
KUNAL <KUNAL@DESKTOP-LCADD3G>
2023-03-03 18:00:07 +0530

    Split Array

---
+++ src/dsaTest1/SplitArrayEqually.java
@@ -0,0 +1,39 @@
+package dsaTest1;
+
+public class SplitArrayEqually {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[] = {1,4,3};
+		System.out.println(splitArray(arr));
+		
+
+	}
+	
+	public static boolean splitArray(int input[]) {
+		return splitArray(input,0,0,0);
+	}
+    
+     public static boolean splitArray(int arr[], int si, int lsum, int rsum) { 
+  
+    	if (si == arr.length) {
+            return lsum == rsum;
+        }
+         
+    	if (arr[si] % 5 == 0){
+            lsum += arr[si];
+            return splitArray(arr,si+1,lsum,rsum);
+        }else if (arr[si] % 3 == 0) {
+            rsum += arr[si]; 
+            return splitArray(arr,si+1,lsum,rsum);
+        }else{
+//        	lsum = lsum+arr[si];
+//        	rsum = rsum+arr[si];
+            return splitArray(arr,si+1, lsum+arr[si], rsum)  || splitArray(arr,si+1,lsum,rsum+arr[si]);
+        }
+         
+    	 
+	} 
+
+}
